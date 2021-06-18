a02a5563a31d641558378c27e8209e62dd2af946
erikkukamezou <erikkukamezoudayo@yahoo.co.jp>
2021-06-18 05:28:53 +0000

    投稿機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/pictures', to: 'pictures#index'
+  resources :pictures
   get 'sessions/new'
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
